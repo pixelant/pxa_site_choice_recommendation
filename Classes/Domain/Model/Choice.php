92f17674841b914393d5398a24322e94069f00d3
anjeylink <andriy.oprysko@resultify.se>
2019-04-11 14:10:30 +0300

    [TASK] add generation of link for choice option

--- Classes/Domain/Model/Choice.php
+++ Classes/Domain/Model/Choice.php
@@ -6,6 +6,7 @@ use TYPO3\CMS\Core\Context\Context;
 use TYPO3\CMS\Core\Site\Entity\SiteLanguage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 
 /***
  *
@@ -240,5 +241,48 @@ class Choice extends AbstractEntity
     public function setPriority(float $priority): void
     {
         $this->priority = $priority;
+    }
+
+    /**
+     * Generate url for select option
+     *
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        // Should be called only FE
+        if (TYPO3_MODE !== 'FE') {
+            return '';
+        }
+
+        $url = '';
+
+        $linkMode = false;
+        // External url
+        if ($this->getLink()) {
+            $parameter = $this->getLink();
+            $linkMode = true;
+        } else {
+            // Or use current page + language UID
+            $parameter = $GLOBALS['TSFE']->id;
+        }
+
+        if ($parameter) {
+            $contentObject = GeneralUtility::makeInstance(ContentObjectRenderer::class);
+            $params = [
+                'parameter' => $parameter,
+            ];
+
+            // If not external URL add language parameter
+            if (false === $linkMode) {
+                $params += version_compare(TYPO3_version, '9.0', '<')
+                    ? ['additionalParams' => '&L=' . $this->getLanguageLayerUid()]
+                    : ['language' => $this->getLanguageLayerUid()];
+            }
+
+            $url = $contentObject->typoLink_URL($params);
+        }
+
+        return $url;
     }
 }
