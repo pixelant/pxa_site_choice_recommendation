81203a180ee585d24cc7043111a1f1f39757ff47
anjeylink <andriy.oprysko@resultify.se>
2019-04-15 10:09:46 +0300

    [TASK] create seperate TS configurations for assets and default configuration

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -3,6 +3,12 @@ defined('TYPO3_MODE') || die('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     'pxa_site_choice_recommendation',
-    'Configuration/TypoScript',
+    'Configuration/TypoScript/Default',
     'Site choice recommendation'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'pxa_site_choice_recommendation',
+    'Configuration/TypoScript/JsAndCss',
+    'Site choice JS and CSS assets'
 );
